32a326ec734b2c6e89d4e0355cb1fba8cf19183c
Mink <ahnmink004@gmail.com>
2019-06-18 01:25:52 +0900

    Updates

---
+++ precache-manifest.73c4f5889c5e9fd23085dd07b0fd06c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd3a261a7bb6b54ab19818612905a17",
+    "url": "/donate_dog/index.html"
+  },
+  {
+    "revision": "d08222ff00dbae1615a0",
+    "url": "/donate_dog/static/css/main.4d11d4bd.chunk.css"
+  },
+  {
+    "revision": "f98c90c4dc4d7aaa0a91",
+    "url": "/donate_dog/static/js/2.714a5e69.chunk.js"
+  },
+  {
+    "revision": "d08222ff00dbae1615a0",
+    "url": "/donate_dog/static/js/main.54b3f693.chunk.js"
+  },
+  {
+    "revision": "ee8d86131c8ad5043700",
+    "url": "/donate_dog/static/js/runtime~main.7a4a8db9.js"
+  },
+  {
+    "revision": "402e4eb49fcaa3fe888210ddd9c2e76a",
+    "url": "/donate_dog/static/media/1212.402e4eb4.jpg"
+  }
+]);
